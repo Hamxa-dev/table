5035bb2083746a91593409451ea841728ec227d7
Hafiz Hamza <hafizhamxa8@gmail.com>
2023-08-24 23:54:25 +0500

    first commit

---
+++ apps.js
@@ -0,0 +1,11 @@
+
+// Table
+
+
+// const num =+ prompt(`Enter is table num put`);
+// const div = document.querySelector(`div`);
+
+// for (let i = 0 ; i <= 10 ; i++){
+//     div.innerHTML +=  `<center> ${num} * ${i} = ${num * i}<br><hr></center>`
+//     console.log (`${num} * ${i} = ${num * i} `)
+// }
